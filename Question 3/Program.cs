918dde663c8f0cf880ef872d0d60a098616bc3f3
AtefehMosh <elina.ee1.8532@gmail.com>
2023-04-16 07:57:13 +0430

    Add project files.

---
+++ Question 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Clac(x, y);
+        }
+        private static void Clac(int x,int y)
+        {
+            double result = Math.Sqrt(Math.Abs(x)) + Math.Pow(y, 2);
+            Console.WriteLine("The Result is : " + result);
+        }
+    }
+}
